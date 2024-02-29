3abf27d01c71a815cf5c5593f68056c2fc2d0c80
William <william@prefinem.com>
2024-02-28 21:14:01 -0600

    Get the correct score and better UI

--- components/CategoryItem.tsx
+++ components/CategoryItem.tsx
@@ -2,19 +2,24 @@ import { Link } from 'expo-router';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { Category, Item } from '../types';
 
-const CategoryItem = ({ category, item }: { category: Category; item: Item }) => {
-  return (
-    <Link href={`/category/${category.categoryId}/item/${item.itemId}`} asChild>
-      <TouchableOpacity>
-        <View className="flex flex-grow flex-row justify-between border-b border-gray-400 py-2">
-          <Text className="text-lg ">{item.itemName}</Text>
-          <View className="items-center justify-center rounded bg-gray-800 px-4">
-            <Text className="text text-white">{item?.ratings ? item?.ratings.length : 0}</Text>
+const CategoryItem = ({ category, item }: { category: Category; item: Item }) => (
+  <Link href={`/category/${category.categoryId}/item/${item.itemId}`} asChild>
+    <TouchableOpacity>
+      <View className="flex items-center justify-between px-4 py-5">
+        <View className="flex min-w-0 flex-row gap-x-4 ">
+          <View className="min-w-0 flex-auto">
+            <Text className="text-xl font-semibold leading-6 text-gray-900">{item.itemName}</Text>
+            <Text className="mt-1 h-[20px] truncate text-lg leading-5 text-gray-500">{item.itemDescription}</Text>
+          </View>
+          <View className="items-center justify-center rounded">
+            <View className="rounded-full border-2 border-gray-400 px-4 py-1">
+              <Text className="text font-bold text-gray-800">{item.overallRating || 0}</Text>
+            </View>
           </View>
         </View>
-      </TouchableOpacity>
-    </Link>
-  );
-};
+      </View>
+    </TouchableOpacity>
+  </Link>
+);
 
 export default CategoryItem;
--- services/category/getCategoryWithItems.ts
+++ services/category/getCategoryWithItems.ts
@@ -3,12 +3,24 @@ import { Category, Item } from '../../types';
 
 const getCategoryWithItems = async (categoryId) => {
   const category: Category = await knex('category').where({ categoryId }).first();
-  const itemIds = await knex('rating').where({ categoryId }).pluck('itemId');
+  const ratings = await knex('rating').select('itemId', 'ratingTotal').where({ categoryId });
+  const itemIds: Partial<Rating> = ratings.map(({ itemId }) => itemId);
   const items: Item[] = itemIds.length > 0 ? await knex('item').whereIn('itemId', itemIds) : [];
+
+  const scores = ratings.reduce(
+    (results, rating) => ({
+      ...results,
+      [rating.itemId]: (results[rating.itemId] || 0) + rating.ratingTotal,
+    }),
+    {},
+  );
 
   return {
     category,
-    items,
+    items: items.map((item) => ({
+      ...item,
+      overallRating: scores[item.itemId],
+    })),
   };
 };
 
